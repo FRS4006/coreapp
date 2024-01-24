8e267c61cae61e82a95996cc67f46c439ec51318
Frederick Scovell <frs4006@med.cornell.edu>
2024-01-24 10:54:57 -0500

    connected user

--- core/models.py
+++ core/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 # discusison topic textfield into file field? 
 # camelcase
 # "container class holding many to many relationship"
 
 class Participant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=30)
     email = models.EmailField(max_length=70)
     studyID = models.CharField(max_length=30)
     startDate = models.DateField()
+
 
     def __str__(self):
         return self.name
